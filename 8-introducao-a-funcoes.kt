45cbcb75022bc30cb51217ebd748116b3b363f3e
Caio Martins <caiofernandes14@gmail.com>
2021-02-10 17:00:02 -0300

    Introdução a funções

---
+++ 8-introducao-a-funcoes.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+}
+
+private fun getFullName(name: String, lastName: String):String {
+    val fulname = "$name $lastName"
+    return fullName
+}
+
+private fun getFullName(name: String, lastName: String):String {
+    return "$name $lastName"
+}
+
+private fun getFullName(name: String, lastName: String) = "$name $lastName"
