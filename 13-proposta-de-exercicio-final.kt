fd690861dcab91881f9caf7ca74e7aa66a76ccc4
Caio Martins <caiofernandes14@gmail.com>
2021-02-11 17:55:50 -0300

    Proposta de exercício final

---
+++ 13-proposta-de-exercicio-final.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    val n1:Float = 1.5f
+    val n2:Float = 2.0f
+    
+    when(5) {
+       1 -> println(n1 + n2)
+       2 -> println(n1 - n2)
+       3 -> println(n1 * n2)
+       4 -> println(n1 / n2)
+       else -> println("Indiferente")
+    }
+    
+}
